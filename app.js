d068921da82cea23af92c09a4ecf9846a8bda8a3
Abhishek Adhikary <abhishek.112031@gmail.com>
2023-09-28 21:48:27 +0530

    implementing call func with dummy url

--- app.js
+++ app.js
@@ -17,26 +17,48 @@ const client = new twilio.Twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWI
 
 // Route to send an SMS
 app.post('/sms', async (req, res) => {
-  const { to } = req.body;
+    const { to } = req.body;
 
-  try {
-    // Send an SMS
-    await client.messages.create({
-      body: 'This is a test SMS from your Twilio-powered server!',
-      from: process.env.TWILIO_PHONE_NUMBER,
-      to,
-    });
+    try {
+        // Send an SMS
+        await client.messages.create({
+            body: 'This is a test SMS from your Twilio-powered server!',
+            from: process.env.TWILIO_PHONE_NUMBER,
+            to,
+        });
 
-    // Respond with success
-    res.status(200).json({ message: 'SMS sent successfully' });
-  } catch (error) {
-    // Handle errors
-    console.error(error);
-    res.status(500).json({ error: 'An error occurred on the server' });
-  }
+        // Respond with success
+        res.status(200).json({ message: 'SMS sent successfully' });
+    } catch (error) {
+        // Handle errors
+        console.error(error);
+        res.status(500).json({ error: 'An error occurred on the server' });
+    }
+});
+
+
+//route to send call:
+app.post('/call', async (req, res) => {
+    const { to } = req.body;
+
+    try {
+        // Make a call
+        const call = await client.calls.create({
+            url: 'http://demo.twilio.com/docs/voice.xml', // TwiML for the call
+            to,
+            from: process.env.TWILIO_PHONE_NUMBER,
+        });
+
+        // Respond with success
+        res.status(200).json({ message: 'Call initiated successfully', callSid: call.sid });
+    } catch (error) {
+        // Handle errors
+        console.error(error);
+        res.status(500).json({ error: 'An error occurred on the server' });
+    }
 });
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
